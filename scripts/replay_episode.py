53d0faa10b704f9fa8429270fabd9717c6a76c6d
mira <michael.razoumovitch@gmail.com>
2025-10-25 16:16:08 +0200

    First version to replay

--- scripts/replay_episode.py
+++ scripts/replay_episode.py
@@ -1,9 +1,12 @@
 import argparse
 
 import logging
-from lerobot.datasets.lerobot_dataset import LeRobotDataset
-from lerobot.datasets.utils import ACTION
+import time
 
+from lerobot.datasets.lerobot_dataset import LeRobotDataset
+from lerobot.utils.robot_utils import busy_wait
+
+from piper_teleop.robot_server.core import RobotInterface
 
 logger = logging.getLogger(__name__)
 
@@ -19,6 +22,28 @@ if __name__ == "__main__":
     dataset = LeRobotDataset(repo_id=args.repo_id, root=args.root, episodes=[args.episode_id])
 
     episode_frames = dataset.hf_dataset.filter(lambda x: x["episode_index"] == args.episode_id)
-    actions = episode_frames.select_columns(ACTION)
-
+    actions = episode_frames.select_columns('action')
+    from piper_teleop.config import TelegripConfig, config
     logger.info(f"Replaying episode {args.episode_id} with {len(actions)} frames")
+    robot = RobotInterface(config)
+    robot.connect()
+    for idx in range(dataset.num_frames):
+        start_episode_t = time.perf_counter()
+
+        action_array = actions[idx]["action"]
+        dict_left = dict()
+        dict_right = dict()
+        for i, name in enumerate(dataset.features["action"]["names"]):
+            action = float(action_array[i])
+            if name[:2] == 'L.':
+                dict_left[name.replace('L.','') + '.pos'] = action
+            elif name[:2] == 'R.':
+                dict_right[name.replace('R.', '') + '.pos'] = action
+            else:
+                raise Exception('wrong name')
+
+        robot.left_robot.send_action(dict_left)
+        robot.right_robot.send_action(dict_right)
+
+        dt_s = time.perf_counter() - start_episode_t
+        busy_wait(1 / dataset.fps - dt_s)
