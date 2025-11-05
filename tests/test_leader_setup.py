f737f0bcc52ea47c9198254ab198bd62b271b9c9
mira <michael.razoumovitch@gmail.com>
2025-11-05 14:30:31 +0100

    Added prototype for Robot Leader

---
+++ tests/test_leader_setup.py
@@ -0,0 +1,27 @@
+import pprint
+import time
+
+import pytest
+from piper_sdk import ArmMsgFeedbackStatusEnum
+
+from piper_teleop.robot_server.core.piper import Piper, PiperConfig
+from piper_teleop.robot_server.robot_leader import PiperLeader
+
+
+@pytest.mark.skip(reason='only for manual testing')
+def test_read_robot_positions():
+    left_config = PiperConfig(port="left_piper", id="left_piper")
+    robot = Piper(left_config)
+    while True:
+        obs = robot.get_observation()
+        pprint.pprint(obs)
+        status = robot.sdk.piper.GetArmStatus()
+        assert status.arm_status.ctrl_mode == ArmMsgFeedbackStatusEnum.CtrlMode.TEACHING_MODE
+        time.sleep(0.1)
+
+def test_leader():
+    leader = PiperLeader()
+    leader.connect()
+    obs = leader.get_observations()
+    assert 'left' in obs
+    assert 'right' in obs
